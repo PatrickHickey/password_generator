9d88662ff9d02563fcaf16c804eea7a4168a630b
PatrickHickey <codewithpat@gmail.com>
2019-11-10 14:08:50 -0800

    Added DOM functions

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
+// DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol
 };
+
+// Generate event listen
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+// Generate password function
+function generatePassword(lower, upper, number, symbol, lenght) {
+  // 1. Init pw var
+  // 2. Filter out unchecked types
+  // 3. Loop over length call generator function for each type
+  // 4. Add final pw to the pw var and return
+}
 
 // Generator functions
 
